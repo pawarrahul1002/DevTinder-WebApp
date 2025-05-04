e9555382db81fcca8ed8f9f03da5d6dd53015348
MSI\Rahul Pawar <pawar.rahul1002@gmail.com>
2025-05-04 14:51:24 +0530

    Create vite + react application Remove unecessary code and create a hello world app Install tailwinf and daisy UI Add Navbar component to App.jsx

---
+++ frontend/README.md
@@ -0,0 +1,6 @@
+# DevTinder
+
+- Create vite + react application
+- Remove unecessary code and create a hello world app
+- Install tailwinf and daisy UI
+- Add Navbar component to App.jsx
---
+++ frontend/src/App.jsx
@@ -0,0 +1,13 @@
+import "./App.css";
+import Navbar from "./components/Navbar";
+
+function App() {
+  return (
+    <>
+      <Navbar />
+      {/* <h2 class="text-3xl font-bold underline">Hello World</h2> */}
+    </>
+  );
+}
+
+export default App;
