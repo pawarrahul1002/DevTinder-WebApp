81c7de7eecf2b890d0c5caa2ecee4717a74288ef
MSI\Rahul Pawar <pawar.rahul1002@gmail.com>
2025-05-04 15:29:24 +0530

    npm i react-router-dom BrowserRouter-Routes-Route- path=/pathname element={<'componentname'/>} outlet for child of route create footer component

--- frontend/README.md
+++ frontend/README.md
@@ -4,3 +4,7 @@
 - Remove unecessary code and create a hello world app
 - Install tailwinf and daisy UI
 - Add Navbar component to App.jsx
+- npm i react-router-dom
+- BrowserRouter-Routes-Route- path=/pathname element={<'componentname'/>}
+- outlet for child of route
+- create footer component
--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,11 +1,21 @@
 import "./App.css";
+import Login from "./components/Login";
 import Navbar from "./components/Navbar";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Profile from "./components/Profile";
+import Body from "./components/Body";
 
 function App() {
   return (
     <>
-      <Navbar />
-      {/* <h2 class="text-3xl font-bold underline">Hello World</h2> */}
+      <BrowserRouter basename="">
+        <Routes>
+          <Route path="/" element={<Body />}>
+            <Route path="/login" element={<Login />} />
+            <Route path="/profile" element={<Profile />} />
+          </Route>
+        </Routes>
+      </BrowserRouter>
     </>
   );
 }
